b7e2ff6b97258d7e825976b70363d2fe75a84d4a
jorishr <joris@jorisr.com>
2020-07-04 12:19:25 +0200

    Update after.js
    Improved comments

--- facade-pattern/after.js
+++ facade-pattern/after.js
@@ -1,4 +1,4 @@
-// fetch the users and there posts from the API, log in the console
+//  The facade
 function getUsers() {
     return doFetch('https://jsonplaceholder.typicode.com/users');
 };
@@ -17,14 +17,17 @@ getUsers().then(users => {
       })
     })
 });
-/* Constructing the queryString:
-    - get the entries of the params object, for each user you get an an array.
-    - the first value: "userId", the second value the id number of the user
-    - the queryString we need: 'userId=<userNumberId>'
-    - map over the arrays and return the queryString
-    - all the different user queryString need to be joined together into one
-    string again and with the & as seperator. Thus you get:
-        `https://jsonplaceholder.typicode.com/posts?userId=1&userId=2&userId=3...etc`
+
+//  Implementation details
+
+/* 
+Constructing the queryString:
+- get the entries of the params object, for each user you get an an array.
+- the first value: "userId", the second value the id number of the user
+- the queryString we need: 'userId=<userNumberId>'
+- map over the arrays and return the queryString
+- all the different user queryString need to be joined together into one
+string again and with the & as seperator. Thus you get: `https://jsonplaceholder.typicode.com/posts?userId=1&userId=2&userId=3...etc`
 */
 function doFetch(url, params = {}) {
     //console.log(Object.entries(params));
@@ -40,10 +43,12 @@ function doFetch(url, params = {}) {
 // can easily be replace by the AXIOS library which has a cleaner syntax and 
 // can handle the params without the need of building the queryString manually.
 
-/* function getFetch(url, params = {}) {
+/* 
+function getFetch(url, params = {}) {
     return axios({
       url: url,
       method: "GET",
       params: params
     }).then(res => res.data);   // instead of res.json
-} */
+} 
+*/
